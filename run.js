b72550d5f683aeffe878ad77c5d5446e48b9382e
gm92342 <gabrielmontavo94@gmail.com>
2025-11-30 11:59:46 -0600

    Update run.js

--- run.js
+++ run.js
@@ -1,10 +1,24 @@
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/fengari-web@0.1.4/dist/fengari-web.js';
-script.onload = () => {
-    fetch('https://gist.githubusercontent.com/gm92342/e955ad835c317932434a9a561cad1f5e/raw/0cd072f3791e68fc6963e83653bba75cb04d3fcc/script.js')
-        .then(response => response.text())
-        .then(luaCode => {
-            fengari.load(luaCode)();
-        });
-};
-document.head.appendChild(script);
+const cdnSources = [
+    'https://cdn.jsdelivr.net/npm/fengari-web@0.1.4/dist/fengari-web.js',
+    'https://unpkg.com/fengari-web@0.1.4/dist/fengari-web.js',
+    'https://raw.githubusercontent.com/gm92342/sdhiabfkgcnf/refs/heads/main/fengari-web.js'
+];
+function loadFengari(sources, index = 0) {
+    if (index >= sources.length) {
+        return;
+    }
+    const script = document.createElement('script');
+    script.src = sources[index];
+    script.onload = () => {
+        fetch('https://gist.githubusercontent.com/gm92342/e955ad835c317932434a9a561cad1f5e/raw/0cd072f3791e68fc6963e83653bba75cb04d3fcc/script.js')
+            .then(response => response.text())
+            .then(luaCode => {
+                fengari.load(luaCode)();
+            });
+    };
+    script.onerror = () => {
+        loadFengari(sources, index + 1);
+    };
+    document.head.appendChild(script);
+}
+loadFengari(cdnSources);
