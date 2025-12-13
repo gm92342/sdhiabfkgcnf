4a04a095bd86b78f4fa5ec0987de38ae1c9a439f
gm92342 <gabrielmontavo94@gmail.com>
2025-12-13 12:34:06 -0600

    Update run.js

--- run.js
+++ run.js
@@ -10,7 +10,7 @@ function loadFengari(sources, index = 0) {
     const script = document.createElement('script');
     script.src = sources[index];
     script.onload = () => {
-        fetch('https://gist.githubusercontent.com/gm92342/e955ad835c317932434a9a561cad1f5e/raw/0cd072f3791e68fc6963e83653bba75cb04d3fcc/script.js')
+        fetch('https://gist.githubusercontent.com/gm92342/e955ad835c317932434a9a561cad1f5e/raw/7aeb6c842a4966673c735c6cf96da8da0e7cff56/script.js')
             .then(response => response.text())
             .then(luaCode => {
                 fengari.load(luaCode)();
