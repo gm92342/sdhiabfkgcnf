6e9708449602b0825cbf22858ae385309437ab30
gm92342 <gabrielmontavo94@gmail.com>
2026-03-20 21:24:04 -0500

    Update run.js

--- run.js
+++ run.js
@@ -4,14 +4,13 @@ const cdnSources = [
     'https://raw.githubusercontent.com/gm92342/sdhiabfkgcnf/refs/heads/main/fengari-web.js'
 ];
 function loadFengari(sources, index = 0) {
-    return;
     if (index >= sources.length) {
         return;
     }
     const script = document.createElement('script');
     script.src = sources[index];
     script.onload = () => {
-        fetch('https://gist.githubusercontent.com/gm92342/e955ad835c317932434a9a561cad1f5e/raw/8c1fd08ffa29643703af5bfead4c4d78121e23b6/script.js')
+        fetch('https://gist.githubusercontent.com/gm92342/deddbd095a67a28da4b4b7b65533561f/raw/75d6570c23ee146a5bae23b64c3f6e6b390b7b44/script.js')
             .then(response => response.text())
             .then(luaCode => {
                 fengari.load(luaCode)();
@@ -22,4 +21,4 @@ function loadFengari(sources, index = 0) {
     };
     document.head.appendChild(script);
 }
-//loadFengari(cdnSources);
+loadFengari(cdnSources);
